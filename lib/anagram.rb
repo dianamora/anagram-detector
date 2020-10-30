40f1f484a496337f7ac632d494cc0442e2717827
Diana Mora <morra.dianna@gmail.com>
2020-10-30 14:46:49 -0400

    commit tree b31e065cc05afd854ddbfa9ff23526c4867e72c8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,37 @@
-# Your code goes here!
+# # Your code goes here!
+# class Anagram
+  
+#     attr_accessor :word
+    
+#     def initialize(word)
+#       @word = word
+#     end
+
+#     def match(words_array)
+#         #takes an array of possible anagrams
+#         #return all matches in an array
+#         #if no matches, return empty array
+#         words_array.select do |element|
+#              word.split("").sort == element.split("").sort
+#             #  words_array
+#             # else 
+#             #   array = []
+#           # end
+#         end
+      
+#     end
+# end
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+def match(words_array)
+  words_array.select do |element|
+       word.split("").sort == element.split("").sort    
+  end
+end
+end
